3107f78efa7093d8b422f680fcbf42e1a41588c1
Sandra Mabel Gonzalez <smg3117@gmail.com>
2025-10-21 22:09:06 -0300

    Casi casi

--- app/models/item.model.php
+++ app/models/item.model.php
@@ -6,13 +6,22 @@ class ItemModel extends Model{
 
     public function getItems() {
         $query = $this->db->prepare('
-            SELECT *
-            FROM prenda
-            ');
+        SELECT 
+            a.id,
+            a.nombre AS prendaNombre, 
+            a.material AS prendaMaterial,
+            a.precio AS prendaPrecio, 
+            a.disponible AS prendaDisponible, 
+            b.nombre AS categoriaNombre
+        FROM prenda a
+        INNER JOIN categoria b
+            ON a.id_categoria = b.id
+        ');
+    
         $query->execute();
-        
         $items = $query->fetchAll(PDO::FETCH_OBJ);
         return $items;
+
     }
 
     public function getCategoryItems($categoryId){
@@ -25,17 +34,22 @@ class ItemModel extends Model{
 
     
     public function getItem($id) {
-        $query = $this->db->prepare('SELECT * FROM prenda WHERE id=?');
-        $query->execute([$id]);
+        $query = $this->db->prepare('
+            SELECT 
+                a.nombre AS prendaNombre,
+                a.material AS prendaMaterial,
+                a.precio AS prendaPrecio,
+                a.disponible AS prendaDisponible,
+                a.descripcion AS prendaDescripcion,
+                b.nombre AS categoriaNombre
+            FROM prenda a
+            INNER JOIN categoria b
+                ON a.id_categoria = b.id
+            WHERE a.id = ?
+        ');
         
+        $query->execute([$id]);
         $item = $query->fetch(PDO::FETCH_OBJ);
-
-        if ($item) {
-            $categoryQuery = $this->db->prepare("SELECT * FROM categoria WHERE id = ?");
-            $categoryQuery->execute([$item->id]);
-            $category  = $categoryQuery->fetch(PDO::FETCH_OBJ);
-            $item->nombre = $category ? $category->nombre : null;
-        }
 
         return $item;
     }
